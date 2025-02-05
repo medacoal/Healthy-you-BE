14529bc429509d6bdfb57fc56ffae4386098ac4f
NofaceNocase-gif <charlesomu6@gmail.com>
2025-02-05 15:39:39 +0100

    middleware

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -5,6 +5,9 @@ import { authCheck } from "../middlewares/auth.js";
 const router = express.Router();
 
 router.post('/register', register)
-router.post('/login',authCheck, login);
+router.post('/login', login);
+router.get("/authcheck", authCheck, (req, res) => {
+    res.json({ success: true, message: "User Authenticated successfully!" })
+});
 
 export default router;
